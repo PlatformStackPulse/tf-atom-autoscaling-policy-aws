84ec25a40faa450818be83e10e1d61af59195492
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-26 23:25:51 +0100

    feat: implement tf-atom-autoscaling-policy-aws module (#1)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "arn" {
+  description = "ARN of the scaling policy"
+  value       = try(aws_autoscaling_policy.this[0].arn, null)
+}
+
+output "name" {
+  description = "Name of the scaling policy"
+  value       = try(aws_autoscaling_policy.this[0].name, null)
 }
